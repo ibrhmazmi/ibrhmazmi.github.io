b76cac74477898b83e2facb7c9e84c0aa6a54fdb
ibrhmazmi <75335119+ibrhmazmi@users.noreply.github.com>
2022-08-24 19:54:26 +0800

    Add files via upload

---
+++ .github/header.php
@@ -0,0 +1,28 @@
+<head>
+<link href='https://fonts.googleapis.com/css?family=Playfair Display' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=League Spartan' rel='stylesheet'>   
+<link href='https://fonts.googleapis.com/css?family=Titillium Web' rel='stylesheet'>
+
+<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/body.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="scss/style.scss">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<nav>
+    
+    <label class="Name icon-3d">aim </label>
+    <input id="btn-nav" type="checkbox">
+    <label for="btn-nav">
+        <img class="btn-nav " src="assets/burger.png">
+    </label>
+    <ul>
+        <li><a href="#" class="active">About Me</a></li>
+        <li><a href="#">Porfolio</a></li>
+        <li><a href="#">Contact ME</a></li>
+    </ul>
+
+</nav>
